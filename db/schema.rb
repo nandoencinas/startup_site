fdb5192022dca44bcb12f0ab91bd105e2408c644
Fernando Encinas <ferencinas@gmail.com>
2014-11-28 10:28:40 +0100

    learning about migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141127155608) do
+ActiveRecord::Schema.define(version: 20141128085117) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20141127155608) do
     t.string   "image_url"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.decimal  "price"
   end
 
 end
